4b0df50927367cfaddd56f202a5f3f40df1cc80c
AlexKMan <keeley.lp@gmail.com>
2019-03-27 20:59:04 +0000

    updated tests for checkbox.test.js

--- src/components/checkbox.test.js
+++ src/components/checkbox.test.js
@@ -2,4 +2,23 @@ import { mount } from "enzyme";
 import React from "react";
 import Checkbox from "./checkbox";
 
-describe("checkbox", () => {});
+describe("checkbox", () => {
+  let component;
+  let mockFn;
+  let id;
+  beforeEach(() => {
+    mockFn = jest.fn();
+    id = "123";
+    component = mount(<Checkbox id={id} onClick={mockFn} />);
+  });
+  it("renders correctly", () => {
+    expect(component).toMatchSnapshot();
+  });
+  it("executes onClick handler on click", () => {
+    component.find("input").simulate("click");
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+  it("has id as given by the prop", () => {
+    expect(component.find({ id }).exists()).toEqual(true);
+  });
+});
