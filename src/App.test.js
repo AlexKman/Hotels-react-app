59715d63fb7ed3a84b5f48fb10da787c94e8afb7
AlexKMan <keeley.lp@gmail.com>
2019-03-27 20:58:20 +0000

    added test to app.test.js for checkbox

--- src/App.test.js
+++ src/App.test.js
@@ -3,4 +3,25 @@ import React from "react";
 import App from "./App";
 import hotels from "./data/hotels.json";
 
-describe("checkbox", () => {});
+describe("checkbox", () => {
+  let component;
+  let mockFn;
+  beforeEach(() => {
+    mockFn = jest.fn();
+    component = mount(<App />);
+  });
+  describe("Component", () => {
+    it("renders correctly", () => {
+      expect(component).toMatchSnapshot();
+    });
+    it("has a select box with two options", () => {
+      expect(component.find("select")).toHaveLength(1);
+    });
+    it("has a select box with two options", () => {
+      expect(component.find("option")).toHaveLength(2);
+    });
+    it("has three CheckBox's", () => {
+      expect(component.find("Checkbox")).toHaveLength(3);
+    });
+  });
+});
