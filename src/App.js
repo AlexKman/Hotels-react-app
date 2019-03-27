8a4763aec0a3a5640d213e62b493d3a312b29b61
AlexKMan <keeley.lp@gmail.com>
2019-03-27 20:43:12 +0000

    imported hotels json file into app, mounted to state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import hotels from "./data/hotels.json";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    hotels: []
+  };
+  componentDidMount() {
+    this.setState({ hotels });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">Hotels react app</header>
+        <header className="App-header">Hotels-react-app</header>
+        <ul />
       </div>
     );
   }
